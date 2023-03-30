2aecab656a93ff63874cc324dd7a89e14570a859
andrez.mazo@gmail.com <andrez.mazo@gmail.com>
2023-03-29 20:44:51 -0500

    Over pic in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,20 +5,22 @@ import pic from "../img/pic.jpg";
 function About(props) {
   return (
     <React.Fragment>
-      <section  ref={props.myRef} className="about-container" id="About">
+      <section ref={props.myRef} className="about-container" id="About">
         <div className="box-container neu">
           <div className="pic-container">
+            <div className="over-pic"></div>
             <img src={pic} className="main-pic" alt="Andrés Zapata Mazo" />
           </div>
           <div className="text-container center">
             <h3 className="title-section">About me</h3>
             <h2>Hello!</h2>
             <p>
-              My name is Andrés, I'm a Sound Engineer and a Junior Full Stack Developer. I enjoy creating things as a Web Developer.
-              I have qualities of adaptability, determination, leadership and
-              teamwork management, which have been my greatest tools in each of
-              the projects where I have worked. I'm looking for opportunitties to acquire new
-              knowledge to develop myself in new challenges
+              My name is Andrés, I'm a Sound Engineer and a Junior Full Stack
+              Developer. I enjoy creating things as a Web Developer. I have
+              qualities of adaptability, determination, leadership and teamwork
+              management, which have been my greatest tools in each of the
+              projects where I have worked. I'm looking for opportunitties to
+              acquire new knowledge to develop myself in new challenges
             </p>
             <p>What would life be if we had no courage to attempt anything?</p>
             <p>- Vincent Van Gogh</p>
